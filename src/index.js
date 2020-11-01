61eee7f08543e6a0a7b5e6696512e1daffdd5155
Daniela Kůsová <danielka.kusova@gmail.com>
2020-11-01 13:45:21 +0100

    Menu funguje

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import './style.css';
 
 console.log('funguju!');
 
+const navigation = document.querySelector('nav');
+
+const navigace = document.getElementById('nav-btn');
+navigace.addEventListener('click', () => {
+  navigation.classList.toggle('nav-closed');
+});
+console.log('jedu!');
+
+const navigaceElm = document.querySelectorAll('.nav-button');
+
+navigaceElm.forEach((polozka) => {
+  polozka.addEventListener('click', () => {
+    navigation.classList.add('nav-closed');
+  });
+});
